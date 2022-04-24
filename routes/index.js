ad880e8d66df0ea535746fe521bc36cb4906b2d8
Roger Davila <rogerddavila@gmail.com>
2022-04-23 21:22:18 -0700

    add fetch capability for card sets

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
 const passport = require('passport');
 
+const rootUrl = 'https://api.scryfall.com/sets/'
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let cardSet;
+  fetch(`${rootUrl}neo/`)
+  .then(res => res.json())
+  .then(set => {
+    cardSet = set;
+    return fetch(set.search_uri)
+  })
+  .then(res => res.json())
+  .then(cards => {
+    console.log(cards);
+    cardSet.cards = cards;
+    res.render('index', {cardSet, title: 'MTG Archive'})
+  });
 });
 
 router.get('/auth/google', passport.authenticate('google', {
