76f4aec4781a3822f79ff00784871d196add76b7
Roger Davila <rogerddavila@gmail.com>
2022-04-25 09:02:11 -0700

    add card model

---
+++ models/card.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const cardSchema = new Schema({
+  user: [{type: Schema.Types.ObjectId, ref: 'User'}],
+  cardDetails: {
+    type: Schema.Types.Mixed,
+    required: true
+  }
+},{
+  timestamps: true
+});
+
+module.exports = mongoose.model('Card', cardSchema);
