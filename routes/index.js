3b4ae99f9da11b48a323452d6d862cc4791eca1c
Roger Davila <rogerddavila@gmail.com>
2022-04-26 09:11:35 -0700

    remove routes that are not a part of  the authentication

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,11 @@ const router = express.Router();
 const fetch = require('node-fetch');
 const passport = require('passport');
 
-const rootUrl = 'https://api.scryfall.com/sets/'
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let cardSet;
-  fetch(`${rootUrl}neo/`)
-  .then(res => res.json())
-  .then(set => {
-    cardSet = set;
-    return fetch(set.search_uri)
-  })
-  .then(res => res.json())
-  .then(cards => {
-    console.log(cards);
-    cardSet.cards = cards;
-    res.render('index', {cardSet, title: 'MTG Archive'})
-  });
+  if (req.user) res.render('decks/index')
+  res.render('index')
 });
 
 router.get('/auth/google', passport.authenticate('google', {
