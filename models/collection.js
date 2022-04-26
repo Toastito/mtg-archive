59084b082c929a5bc4249b4a8e7d8e6900ab166f
Roger Davila <rogerddavila@gmail.com>
2022-04-26 09:28:59 -0700

    add price function to sum collection value

--- models/collection.js
+++ models/collection.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const collectionSchema = new Schema({
-  author: {type: Schema.Types.ObjectId, ref: 'User'},
+  owner: {type: Schema.Types.ObjectId, ref: 'User'},
   cards: [{type: Schema.Types.ObjectId, ref: 'Card'}],
-  cost: () => {} 
+  value: () => {
+    this.cards.reduce((sum, card) => {
+      sum += parseFloat(card.prices.usd || 0);
+    }, 0)
+  }
 }, {
   timestamps: true
 });
+
+module.exports = mongoose.model('Collection', collectionSchema);
