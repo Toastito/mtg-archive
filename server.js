295f06339156c98e452d383b5536707cd65d376c
Roger Davila <rogerddavila@gmail.com>
2022-04-26 09:32:57 -0700

    add card router

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ require('./config/passport');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
 
 const app = express();
 
@@ -41,6 +42,7 @@ app.use((req, res, next) => {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', cardsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
