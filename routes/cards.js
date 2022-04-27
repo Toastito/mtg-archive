cdc494b41ee45b2b32ff335609ef512c11b515e2
Roger Davila <rogerddavila@gmail.com>
2022-04-27 09:27:31 -0700

    add route for card search page

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
 
-router.get('/cards/search', (req, res, next) => {
-  res.render('cards/index');
+router.get('/cards/search', async (req, res, next) => {
+  let results = await fetch(`https://api.scryfall.com/cards/search?q=${req.query.q}`);
+  results = await results.json();
+  res.render('cards/index', { cards: results.data });
 });
 
 module.exports = router;
