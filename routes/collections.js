88506adb847a57a96d7367c4a0dc277d6b0b3175
Roger Davila <rogerddavila@gmail.com>
2022-04-28 15:01:42 -0700

    add update card functionality

--- routes/collections.js
+++ routes/collections.js
@@ -31,17 +31,39 @@ router.post('/collections', async (req, res) => {
 
 router.put('/collections', async (req, res) => {
   let collection = await Collection.findById(req.body.collection);
-  if (!collection) res.redirect('/cards/search');
-  let collectionCard = await Card.findOne({'cardDetails.id': req.body.cardId});
-  collectionCard.user.push(res.locals.user.id);
-  await collectionCard.save();
-  collection.cards.push({quantity: req.body.quantity, card: collectionCard});
-  await collection.save();
   console.log(collection);
   console.log(req.body);
+  if (!collection) res.redirect('/cards/search');
+  collection.cards.forEach(async (card, idx) => {
+    await collection.populate(`cards.${idx}.card`);
+  });
+  await collection.save();
+  let cardIdx = collection.cards.findIndex((card) => card.card.cardDetails.id === req.body.cardId);
+  console.log(cardIdx);
+  // Ternary for collection if card exists
+  let collectionCard = await Card.findOne({'cardDetails.id': req.body.cardId});
+  console.log(collectionCard);
+  let cardInCollections = collectionCard.user.some((user) => user.equals(res.locals.user.id)); 
+  if (cardIdx === -1) {
+    collection.cards.push({quantity: parseInt(req.body.quantity), card: collectionCard});
+    await collection.save();
+  } else if(cardIdx >= 0) {
+    console.log(`The card was found in index : ${cardIdx}`);
+    console.log('Updating the Card Quantity');
+    collection.cards[cardIdx].quantity += parseInt(req.body.quantity);
+    await collection.save();
+  }
+  // if (!cardIdx) {
+
+    // collectionCard.user.push(res.locals.user.id);
+    // await collectionCard.save();
+
+  // }
+  // console.log(req.body);
   // let card = await Card.findOne({cardDetails: req.body.cardId});
   // console.log(card);
-  res.status(204).send();
+  res.redirect('cards/search');
+  // res.status(204).send();
 });
 
 module.exports = router;
