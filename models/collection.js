ff7db81d79cba970fe317e01dfd338486ffe3f9a
Roger Davila <rogerddavila@gmail.com>
2022-04-27 23:25:19 -0700

    collection card schema for multiple card quantities

--- models/collection.js
+++ models/collection.js
@@ -1,20 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const collectionCardSchema = new Schema({
+  quantity: {
+    type: Number,
+    min: 1,
+    default: 1
+  },
+  card: {type: Schema.Types.ObjectId, ref: 'Card'}
+});
+
 const collectionSchema = new Schema({
   collectionName: {
     type: String,
     required: true
   },
   owner: {type: Schema.Types.ObjectId, ref: 'User'},
-  cards: [{
-    card:{type: Schema.Types.ObjectId, ref: 'Card'},
-    quantity: {
-      type: Number,
-      min: 1,
-      default: 1
-    }
-  }],
+  cards: [collectionCardSchema],
   // collectionValue: function () {
   //   this.cards.reduce((sum, card) => {
   //     sum += parseFloat(card.prices.usd || 0);
