d221e7de1877067cf765b14126ed6df3cbbf2df8
Roger Davila <rogerddavila@gmail.com>
2022-04-27 22:44:51 -0700

    add conditional is user is logged in to retrieve their collections

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
+const Collection = require('../models/collection');
 
 router.get('/cards/search', async (req, res, next) => {
   let results = await fetch(`https://api.scryfall.com/cards/search?q=${req.query.q}`);
   results = await results.json();
+  if (res.locals.user) {
+    let collections = await Collection.find({owner: req.locals.user.id});
+    console.log(collections);
+  }
   res.render('cards/index', { cards: results.data });
 });
 
