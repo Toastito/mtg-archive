698f5108725dfc9aab2f0b423201f74e83d857ce
Roger Davila <rogerddavila@gmail.com>
2022-04-29 15:50:43 -0700

    protect routes with login requirement

--- routes/cards.js
+++ routes/cards.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const cardsCtrl = require('../controllers/cards');
+const isLoggedIn = require('../config/auth');
 
 router.get('/cards/search', cardsCtrl.index);
 
-router.post('/cards', cardsCtrl.create);
+router.post('/cards', isLoggedIn, cardsCtrl.create);
 
-router.put('/cards/:id', cardsCtrl.update);
+router.put('/cards/:id', isLoggedIn, cardsCtrl.update);
 
-router.delete('/cards/:id', cardsCtrl.delete);
+router.delete('/cards/:id', isLoggedIn, cardsCtrl.delete);
 
 module.exports = router;
--- routes/collections.js
+++ routes/collections.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const collectionsCtrl = require('../controllers/collections');
+const isLoggedIn = require('../config/auth');
 
-router.get('/collections', collectionsCtrl.index);
+router.get('/collections', isLoggedIn, collectionsCtrl.index);
 
 router.get('/collections/all', collectionsCtrl.allCollections);
 
 router.get('/collections/:id', collectionsCtrl.show);
 
-router.post('/collections', collectionsCtrl.create);
+router.post('/collections', isLoggedIn, collectionsCtrl.create);
 
-router.delete('/collections/:id', collectionsCtrl.delete);
+router.delete('/collections/:id', isLoggedIn, collectionsCtrl.delete);
 
 module.exports = router;
