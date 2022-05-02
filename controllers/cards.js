7cc7db1b3a3ab55c4f3eef060814dcc58f82223d
Roger Davila <rogerddavila@gmail.com>
2022-05-02 08:49:06 -0700

    change user variable from res.locals.user.id to req.user._id

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,7 +2,6 @@ const Collection = require('../models/collection');
 const Card = require('../models/card');
 const fetch = require('node-fetch');
 
-
 module.exports = {
   index,
   create,
@@ -16,13 +15,13 @@ async function index(req, res) {
   
   let collections;
   if (res.locals.user) {
-    collections = await Collection.find({ owner: res.locals.user.id });
+    collections = await Collection.find({ owner: req.user._id });
   }
   res.render('cards/index', { cards: results.data, collections });
 }
 
 async function create(req, res) {
-  let collection = await Collection.findOne({ _id: req.body.collection, owner: res.locals.user.id }).populate('owner').populate('cards.card').exec();
+  let collection = await Collection.findOne({ _id: req.body.collection, owner: req.user._id }).populate('owner').populate('cards.card').exec();
   if (!collection) res.redirect('/cards/search');
 
   let cardIdx = collection.cards.findIndex((card) => card.card.cardDetails.id === req.body.cardId);
@@ -39,14 +38,15 @@ async function create(req, res) {
 }
 
 async function update(req, res) {
-  let collection = await Collection.findOne({ owner: res.locals.user.id, 'cards._id': req.params.id });
+  let collection = await Collection.findOne({ owner: req.user._id, 'cards._id': req.params.id });
   await collection.cards.id(req.params.id).set({ quantity: req.body.quantity });
   await collection.save();
   res.redirect(`/collections/${collection._id}`);
 }
 
 async function deleteCard(req, res) {
-  let collection = await Collection.findOne({ owner: res.locals.user.id, 'cards._id': req.params.id });
+
+  let collection = await Collection.findOne({ owner: req.user._id, 'cards._id': req.params.id });
   await collection.cards.remove(req.params.id);
   await collection.save();
   res.redirect(`/collections/${collection._id}`);
