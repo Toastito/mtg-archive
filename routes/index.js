12db602509d93916fc2452d1e62b0faac3dcdc97
Roger Davila <rogerddavila@gmail.com>
2022-05-02 22:08:07 -0700

    add recent sets to index page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-
+const fetch = require('node-fetch');
+const rootUrl = 'http://api.scryfall.com';
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/', async (req, res, next) => {
+  let recentSets = await fetch(`${rootUrl}/sets?set_type=expansion`);
+  recentSets = await recentSets.json();
+  recentSets = recentSets.data.filter((set) => {
+    console.log(set.name);
+    console.log(set.released_at);
+    console.log(new Date().getFullYear() - 2);
+    return (set.set_type === 'expansion' || set.set_type === 'funny') && ((new Date().getFullYear() - 2).toString() <= set.released_at);
+  });
+  console.log(recentSets);
+  res.render('index', { recentSets });
 });
 
 router.get('/auth/google', passport.authenticate('google', {
